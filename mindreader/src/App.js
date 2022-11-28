a85dc5f5b7ec6ef416afaec8f07a8f7ea766af15
yebin0310 <yebin060310@gmail.com>
2022-11-28 21:11:34 +0900

    commit

---
+++ mindreader/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+}
+
+export default App;
